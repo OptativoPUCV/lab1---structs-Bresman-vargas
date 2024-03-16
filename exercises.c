1962d26c010516a856480eb9fd4443c444b81f3a
Your Name <you@example.com>
2024-03-16 17:12:40 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -88,14 +88,22 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
 int checkSorted(int arr[], int size) { 
-  for(int i = 0; i < size - 1; i++){
-    if(arr[i] > arr[i+1]){
-      return 0;
-    } 
-    
-    
+  int ascendente = 1;
+  int descendente = 1;
+  for(int i = 0; i < size ; i++){   //No podemos comprar con el último
+    if(arr[i] > arr[i + 1]){        //Ya no es ascendente
+      ascendente = 0;
+    } else if(arr[i] < arr[i + 1]){ //Ya no es descendente
+      descendente = 0;
+    }
   }
-  return -2; 
+  if(ascendente == 1){
+    return 1;
+  } else if(descendente == 1){
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 /*
