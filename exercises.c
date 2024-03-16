b6831f035a0b101fdf7381c065c6f25f45130db1
Your Name <you@example.com>
2024-03-16 17:28:00 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -132,8 +132,8 @@ void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
   (*libro).autor.anioNacimiento = anioNacimiento;
   (*libro).anioPublicacion = anioPublicacion;
 //Punteros a vectores de texto
-  strcopy((*libro).titulo, titulo);
-  strcopy((*libro).autor.nombre, nombreAutor);
+  strcpy((*libro).titulo, titulo);
+  strcpy((*libro).autor.nombre, nombreAutor);
 }
 
 /*
